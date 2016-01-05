885c387626742d745e24c6997d91673710b61938
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-01-04 20:10:04 -0800

    added comments. commented out meat of characterEscapes function so it doesn't break everything else before students fix it

--- rawstrings.js
+++ rawstrings.js
@@ -9,28 +9,31 @@ var printLetter1 = function () {
   return 'print the fourth letter in this string';
 };
 
+// Find and use a method that creates one string out of the two strings in the function body
+// Output: 'Javascript is so much fun I wish I could code  in javascript all day, every day!'
 var bridgeString1 = function() {
-  'Javascript is so much fun'
+  'Javascript is so much fun.'
   'I wish I could code  in javascript all day, every day!'
 
   return
 };
 
+// this function seems out of place - move to another deck?
 var characterEscapes = function() {
-  'I can't think of it right now. I don't have any idea.'
+//   'I can't think of it right now. I don't have any idea.'
 };
 
 /////////////////// STRING METHODS /////////////////////
 
 /*
-SEARCH THE INTERNET FOR 'JAVASCRIPT STRING METHODS'. 
+SEARCH THE INTERNET FOR 'JAVASCRIPT STRING METHODS'.
 FIND AND USE METHODS TO SOLVE THE FOLLOWING PROBLEMS
 */
 
 
 // Create a function that returns the 10th letter of the string:
   // 'print the tenth letter in this string'
-    // Output: 
+    // Output:
 var printLetter2 = function() {
   return 'print the twelfth letter in this string';
 };
@@ -43,8 +46,8 @@ var bridgeString2 = function() {
   return
 };
 
+// Remove the word 'Python' and replace it with 'JavaScript'
 var replaceString = function() {
-  // Remove the word 'Python' and replace it with 'JavaScript'
   'I wish I could code in Python all day, every day!'
 };
 
