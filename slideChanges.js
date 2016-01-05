987e105b376d2d8b9f588485df00e964b569ac3b
Shanna Sullivan <shanna.sullivan@gmail.com>
2016-01-04 19:20:15 -0800

    added slide changes for slide 7 - primitive data types

---
+++ slideChanges.js
@@ -0,0 +1,20 @@
+// JS Data Types Review
+// slide 7
+
+	// Primitive Data Types
+	'hello world' // string
+	'hello world'[6] // w
+
+	5 // integer
+	5 * 5 // 25
+
+	true // boolean
+	!true // false
+
+	null // null
+	!null // true
+	!! null // false
+
+	undefined // undefined
+	!undefined // true
+	!! undefined // false
